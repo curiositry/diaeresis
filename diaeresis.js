193cff8344bffe885119420d365535f2f4b92f8e
Curiositry <isa@automathic.org>
2016-12-11 15:37:30 -0800

    :construction: Add initial config object with WIP dictionary of replacements.

--- diaeresis.js
+++ diaeresis.js
@@ -1,4 +1,29 @@
 /* Diaeresis.js | Copyright (C) 2016 Curiositry (http://curiositry.com) | MIT Licensed */
 (function(){
-
+  var defaultConfig = {
+      selector: "diaeresis",
+      dictionary: {
+        "naive": "naïve",
+        "cooperat": "coöperat",
+        "cooperatives": "coöperatives",
+        "reestablish": "reëstablish",
+        "preeminen": "preëminen",
+        "eelect": "eëlect",
+        "reenter": "reënter",
+        "preeminent": "preëminent",
+        "preempt": "preëmpt",
+        "coordinat": "coördinat",
+        "reengineer": "reëngineer",
+        "reestablish": "reëstablish",
+        "aioli": "aïoli",
+        "schrodinger": "schrödinger",
+        "zoolog": "zoölog",
+        "mosaic": "mosaïc",
+        "reexamin": "reëxamin",
+        "deemphasiz": "deëmphasiz",
+        "reenlist": "reënlist",
+        "microorganism": "microörganism",
+        "dais": "daïs"
+    }
+  };
 })();
